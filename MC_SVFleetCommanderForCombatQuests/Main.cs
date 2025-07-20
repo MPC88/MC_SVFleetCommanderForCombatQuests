c76aff4a6ebb2311760bddf0d5a5dbc2b429e140
MPC88 <myemail@gmail.com>
2025-07-20 11:58:14 +0100

    Updated quest list reference.

--- MC_SVFleetCommanderForCombatQuests/Main.cs
+++ MC_SVFleetCommanderForCombatQuests/Main.cs
@@ -2,6 +2,7 @@
 using BepInEx;
 using HarmonyLib;
 using UnityEngine;
+using static System.Collections.Specialized.BitVector32;
 
 namespace MC_SVFleetCommanderForCombatQuests
 {
@@ -10,7 +11,7 @@ namespace MC_SVFleetCommanderForCombatQuests
     {
         public const string pluginGuid = "mc.starvalor.fleetcommanderforcombatquests";
         public const string pluginName = "SV Fleet Commander for Combat Quests";
-        public const string pluginVersion = "1.0.1";
+        public const string pluginVersion = "1.0.2";
 
         public void Awake()
         {
@@ -46,7 +47,7 @@ namespace MC_SVFleetCommanderForCombatQuests
         [HarmonyPostfix]
         private static void DockUIOpenStationQuest_Post(DockingUI __instance, int index)
         {
-            Quest quest = __instance.station.quests[index].quest;
+            Quest quest = (__instance.station.GetModule(typeof(SM_MissionBoard)) as SM_MissionBoard).quests[index].quest;
             int requirement = QuestDB.GetKnowledgeRequirement(quest);
 
             if (quest.kind == QuestKind.Combat && 
